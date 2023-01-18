44434a364c66004b2a1ccfb097fa3f74004b53c5
Rachel Saini <Css@kirodewal.eu.org>
2023-01-18 17:40:09 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -22,7 +22,7 @@ AUTH = "754495556"
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 userbot = bot
 phonenum = '+918742089059'
-client.connect()
+bot.connect()
 user=client.get_entity(phonenum)
 
 Bot = bot
